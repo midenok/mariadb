25291ca1e7b78cf5cd253a4f7b4af55f3e80ebe0
Aleksey Midenkov <midenok@gmail.com>
2018-03-27 20:29:47 +0300

    [BRANCH START] test_quick_select()

--- sql/table.cc
+++ sql/table.cc
@@ -8655,8 +8655,12 @@ bool TR_table::query(ulonglong trx_id)
   if ((error= setup_conds(thd, this, dummy, &conds)))
     return false;
   select= make_select(table, 0, 0, conds, NULL, 0, &error);
-  if (error || !select)
+  if (error)
+  {
+    my_error(ER_OUT_OF_RESOURCES, MYF(0));
     return false;
+  }
+  DBUG_ASSERT(select);
   // FIXME: (performance) force index 'transaction_id'
   error= init_read_record(&info, thd, table, select, NULL,
                           1 /* use_record_cache */, true /* print_error */,
@@ -8686,12 +8690,19 @@ bool TR_table::query(MYSQL_TIME &commit_time, bool backwards)
     conds= newx Item_func_ge(thd, field, value);
   else
     conds= newx Item_func_le(thd, field, value);
-  if ((error= setup_conds(thd, this, dummy, &conds)))
+  if (setup_conds(thd, this, dummy, &conds))
     return false;
-  // FIXME: (performance) force index 'commit_timestamp'
   select= make_select(table, 0, 0, conds, NULL, 0, &error);
-  if (error || !select)
+  if (error)
+  {
+    my_error(ER_OUT_OF_RESOURCES, MYF(0));
     return false;
+  }
+  DBUG_ASSERT(select);
+  if (process_index_hints(table))
+    return false;
+  int quick= select->test_quick_select(thd, table->s->keys_in_use, 0, UINT_MAX32,
+                            false, false, false);
   error= init_read_record(&info, thd, table, select, NULL,
                           1 /* use_record_cache */, true /* print_error */,
                           false /* disable_rr_cache */);
