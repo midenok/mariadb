8ab183902896eca822125a25dc51295f88c2887e
Aleksey Midenkov <midenok@gmail.com>
2021-11-02 11:13:21 +0300

    MDEV-25004 Missing row in FTS_DOC_ID_INDEX during DELETE HISTORY
    1. In case of system-versioned table add row_end into FTS_DOC_ID index
       in fts_create_common_tables() and innobase_create_key_defs().
       fts_n_uniq() returns 1 or 2 depending on whether the table is
       system-versioned.
    
       After this patch recreate of FTS_DOC_ID index is required for
       existing system-versioned tables. If you see this message in error
       log or server warnings: "InnoDB: Table db/t1 contains 2 indexes
       inside InnoDB, which is different from the number of indexes 1
       defined in the MariaDB" use this command to fix the table:
    
          ALTER TABLE db.t1 FORCE;
    
    2. Fix duplicate history for secondary unique index like it was done
       in MDEV-23644 for clustered index (932ec586aad). In case of
       existing history row which conflicts with currently inseted row we
       check in row_ins_scan_sec_index_for_duplicate() whether that row
       was inserted as part of current transaction. In that case we
       indicate with DB_FOREIGN_DUPLICATE_KEY that new history row is not
       needed and should be silently skipped.
    
    3. Some parts of MDEV-21138 (7410ff436e9) reverted. Skipping of
       FTS_DOC_ID index for history rows made problems with purge
       system. Now this is fixed differently by p.2.
    
    4. wait_all_purged.inc checks that we didn't affect non-history rows
       so they are deleted and purged correctly.
    
    Additional FTS fixes
    
      fts_init_get_doc_id(): exclude history rows from max_doc_id
      calculation. fts_init_get_doc_id() callback is used only for crash
      recovery.
    
      fts_add_doc_by_id(): set max value for row_end field.
    
      fts_read_stopword(): stopwords table can be system-versioned too. We
      now read stopwords only for current data.
    
      row_insert_for_mysql(): exclude history rows from doc_id validation.
    
      row_merge_read_clustered_index(): exclude history_rows from doc_id
      processing.
    
      fts_load_user_stopword(): for versioned table retrieve row_end field
      and skip history rows. For non-versioned table we retrieve 'value'
      field twice (just for uniformity).

--- storage/innobase/row/row0merge.cc
+++ storage/innobase/row/row0merge.cc
@@ -491,6 +491,7 @@ row_merge_buf_redundant_convert(
 @param[in,out]	v_heap		heap memory to process data for virtual column
 @param[in,out]	my_table	mysql table object
 @param[in]	trx		transaction object
+@param[in]	history_row	row is historical
 @return number of rows added, 0 if out of space */
 static
 ulint
@@ -507,7 +508,8 @@ row_merge_buf_add(
 	dberr_t*		err,
 	mem_heap_t**		v_heap,
 	TABLE*			my_table,
-	trx_t*			trx)
+	trx_t*			trx,
+	const bool		history_row)
 {
 	ulint			i;
 	const dict_index_t*	index;
@@ -566,6 +568,7 @@ error:
 		/* Process the Doc ID column */
 		if (!v_col && *doc_id
 		    && col->ind == index->table->fts->doc_col) {
+			ut_ad(!history_row);
 			fts_write_doc_id((byte*) &write_doc_id, *doc_id);
 
 			/* Note: field->data now points to a value on the
@@ -616,7 +619,7 @@ error:
 
 
 			/* Tokenize and process data for FTS */
-			if (index->type & DICT_FTS) {
+			if (!history_row && (index->type & DICT_FTS)) {
 				fts_doc_item_t*	doc_item;
 				byte*		value;
 				void*		ptr;
@@ -1732,6 +1735,7 @@ row_merge_read_clustered_index(
 	char			new_sys_trx_end[8];
 	byte			any_autoinc_data[8] = {0};
 	bool			vers_update_trt = false;
+	bool			history_row = false;
 
 	DBUG_ENTER("row_merge_read_clustered_index");
 
@@ -2161,6 +2165,12 @@ end_of_index:
 					   row_heap);
 		ut_ad(row);
 
+		if (new_table->versioned()) {
+			const dfield_t* dfield = dtuple_get_nth_field(
+				row, new_table->vers_end);
+			history_row = dfield->vers_history_row();
+		}
+
 		for (ulint i = 0; i < n_nonnull; i++) {
 			dfield_t*	field	= &row->fields[nonnull[i]];
 
@@ -2191,6 +2201,7 @@ end_of_index:
 
 		/* Get the next Doc ID */
 		if (add_doc_id) {
+			ut_ad(!history_row);
 			doc_id++;
 		} else {
 			doc_id = 0;
@@ -2225,13 +2236,6 @@ end_of_index:
 
 			ut_ad(add_autoinc
 			      < dict_table_get_n_user_cols(new_table));
-
-			bool history_row = false;
-			if (new_table->versioned()) {
-				const dfield_t* dfield = dtuple_get_nth_field(
-				    row, new_table->vers_end);
-				history_row = dfield->vers_history_row();
-			}
 
 			dfield_t* dfield = dtuple_get_nth_field(row,
 								add_autoinc);
@@ -2358,7 +2362,8 @@ write_buffers:
 					buf, fts_index, old_table, new_table,
 					psort_info, row, ext, &doc_id,
 					conv_heap, &err,
-					&v_heap, eval_table, trx)))) {
+					&v_heap, eval_table, trx,
+					history_row)))) {
 
 				/* Set the page flush observer for the
 				transaction when buffering the very first
@@ -2691,7 +2696,8 @@ write_buffers:
 						buf, fts_index, old_table,
 						new_table, psort_info, row, ext,
 						&doc_id, conv_heap,
-						&err, &v_heap, eval_table, trx)))) {
+						&err, &v_heap, eval_table, trx,
+						history_row)))) {
                                         /* An empty buffer should have enough
                                         room for at least one record. */
 					ut_ad(err == DB_COMPUTE_VALUE_FAILED
