872b558b07e18d44aa8a972edbe3a7154941acc5
Aleksey Midenkov <midenok@gmail.com>
2022-02-19 16:34:08 +0300

    Refactoring: TABLE::referenced_by_foreign_table()

--- sql/table.h
+++ sql/table.h
@@ -1778,6 +1778,7 @@ public:
   void vers_update_fields();
   void vers_update_end();
   void find_constraint_correlated_indexes();
+  bool referenced_by_foreign_table(THD *thd, FOREIGN_KEY_INFO *&fk_info) const;
 
 /** Number of additional fields used in versioned tables */
 #define VERSIONING_FIELDS 2
