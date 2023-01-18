c39bae2f993d2a4ea0f2c5f918f61db58dc6f483
Aleksey Midenkov <midenok@gmail.com>
2023-01-18 16:52:14 +0300

    MDEV-29791 ER_NO_SUCH_TABLE_IN_ENGINE or "File not found" 	   upon concurrent DDL with CREATE OR REPLACE
    Race condition happens when atomic C-O-R drops backup table by
    replaying ddl_log_state_rm in finalize_ddl(). When it already dropped
    table (DDL_DROP_PHASE_TABLE) and before DDL_DROP_PHASE_TRIGGER
    triggers still exist. At that point another thread tries to drop the
    trigger and finds no such table.
    
    The fix protects triggers with MDL lock when renaming original table
    to backup. Thus we can drop triggers in another thread either before
    that and rename to backup will be already without triggers or after
    the C-O-R is finished and at that point there will be no table nor
    triggers.

--- sql/sql_table.h
+++ sql/sql_table.h
@@ -145,9 +145,11 @@ struct rename_param
   LEX_CUSTRING old_version;
   handlerton *from_table_hton;
   int rename_flags; /* FN_FROM_IS_TMP, FN_TO_IS_TMP, etc */
+  bool lock_triggers; /* Acquire MDL_EXCLUSIVE for triggers */
   rename_param() :
     from_table_hton(NULL),
-    rename_flags(0) {}
+    rename_flags(0),
+    lock_triggers(false) {}
 
   int reverse_flags()
   {
