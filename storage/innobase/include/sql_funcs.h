6738a4b9efbbed8d07db9adcbb4c55f17d1e4f38
Aleksey Midenkov <midenok@gmail.com>
2024-09-02 13:47:31 +0300

    CONVERT OUT fix + tests

--- storage/innobase/include/sql_funcs.h
+++ storage/innobase/include/sql_funcs.h
@@ -74,7 +74,18 @@ R"===(PROCEDURE RENAME_CONSTRAINT_IDS () IS
       IF (:old_is_tmp > 0 AND offset > 0) THEN
         foreign_id := CONCAT(SUBSTR(foreign_id2, 0, offset - 1),
                              SUBSTR(foreign_id2, offset2, id_len - offset2));
-        id_len := id_len - 1;
+        id_len := LENGTH(foreign_id);
+      END IF;
+      IF (:old_is_part > 0) THEN
+        offset := INSTR(foreign_id, ')===" "\xFF" R"===(');
+        IF (offset > 0) THEN
+          foreign_id := CONCAT(SUBSTR(foreign_id, 0, offset - 1));
+          id_len := LENGTH(foreign_id);
+        END IF;
+      END IF;
+      IF (:new_is_part > 0) THEN
+        foreign_id := CONCAT(foreign_id, ')===" "\xFF" R"===(', :new_part);
+        id_len := LENGTH(foreign_id);
       END IF;
       IF (INSTR(foreign_id, '/') > 0) THEN
         IF (INSTR(foreign_id,
